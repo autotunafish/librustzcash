820487f2aaca6559668649072ad54b803bace998
autotunafish <autotunafish@users.noreply.github.com>
2026-01-30 21:12:55 +0000

    deploy: 0c64b0575a55a09b4591b11c874b2b4f048f6a4b

---
+++ rustdoc/latest/trait.impl/zcash_client_backend/data_api/trait.WalletWrite.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["zcash_client_backend",[]],["zcash_client_memory",[["impl&lt;P: Parameters&gt; <a class=\"trait\" href=\"zcash_client_backend/data_api/trait.WalletWrite.html\" title=\"trait zcash_client_backend::data_api::WalletWrite\">WalletWrite</a> for <a class=\"struct\" href=\"zcash_client_memory/struct.MemoryWalletDb.html\" title=\"struct zcash_client_memory::MemoryWalletDb\">MemoryWalletDb</a>&lt;P&gt;",0]]],["zcash_client_sqlite",[["impl&lt;C: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;Connection&gt;, P: Parameters, CL: <a class=\"trait\" href=\"zcash_client_sqlite/util/trait.Clock.html\" title=\"trait zcash_client_sqlite::util::Clock\">Clock</a>, R: <a class=\"trait\" href=\"https://rust-random.github.io/rand/rand_core/trait.RngCore.html\" title=\"trait rand_core::RngCore\">RngCore</a>&gt; <a class=\"trait\" href=\"zcash_client_backend/data_api/trait.WalletWrite.html\" title=\"trait zcash_client_backend::data_api::WalletWrite\">WalletWrite</a> for <a class=\"struct\" href=\"zcash_client_sqlite/struct.WalletDb.html\" title=\"struct zcash_client_sqlite::WalletDb\">WalletDb</a>&lt;C, P, CL, R&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[27,376,816]}
